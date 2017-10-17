4bdee9238ceeb8a7bb1feb791230259732498c11
julientr <julientr@ensipc460.imag.fr>
2017-10-17 16:35:51 +0200

    [ex2] Sort Insert Dicho done

--- exercice_2/src/DichoInsertSort.java
+++ exercice_2/src/DichoInsertSort.java
@@ -1,5 +1,39 @@
 package src;
 
-public class DichoInsertSort {
-
+public class DichoInsertSort extends Sort{
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void sort(int[] tab){
+		super.sort(tab);
+		int indSort, tmp;
+		int a, b, m;
+		
+		for (int i=tab.length-2 ; i >= 0 ; i--){			
+			// Recherche dichotomique pour trouver la position d insertion
+			a = i+1;
+			b = tab.length - 1;
+			
+			while (a <= b){
+				m = (a+b)/2;
+				if (tab[i] > tab[m]){
+					a = m+1;
+				}
+				else{
+					b = m-1;
+				}
+			}
+			indSort = (a+b)/2;
+			
+			// Decalage des valeurs pour l insertion
+			tmp = tab[i];
+			for (int j=i ; j < indSort ; j++){
+				tab[j] = tab[j+1];
+			}
+			tab[indSort] = tmp;
+		}
+	}
+	
 }
