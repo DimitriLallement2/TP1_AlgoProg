e0ee93e0035c357c55fe8849f0bbae6d9deda48b
lallemed <lallemed@ensipc160.imag.fr>
2017-10-20 17:32:00 +0200

    Random array generator done Unit test added

--- exercice_1/test/Exo1Test.java
+++ exercice_1/test/Exo1Test.java
@@ -8,16 +8,6 @@ public class Exo1Test extends TestCase {
 	// Liste de test
 	CircularList<Candidate> list = new CircularList<>();
 
-	public final void testOutOfBound() {
-		CircularList<Candidate> list = new CircularList<>();
-		Candidate test;
-		for (int i = 1; i <= 7; i++) {
-			test = new Candidate("" + i, "");
-			list.add(test);
-		}
-		// list.electItem(8);
-		// TODO FINISH
-	}
 
 	/**
 	 * Test correspondant a la question d)
@@ -31,5 +21,38 @@ public class Exo1Test extends TestCase {
 		Candidate expected = new Candidate("4", "");
 		Candidate result = list.electItem(3);
 		assertEquals("7 candidats, k= 3", expected.getName(), result.getName());
+	}
+	
+	/**
+	 * K superieur a la taille de la liste
+	 */
+	public final void testIllegalParamater() {
+		CircularList<Candidate> list = new CircularList<>();
+		Candidate test;
+		for (int i = 1; i <= 7; i++) {
+			test = new Candidate("" + i, "");
+			list.add(test);
+		}
+		 try {
+				list.electItem(8);
+			    fail("Devrait lever une exception quand k est superieur a la taille de la liste");
+			  }catch(IllegalArgumentException e){
+			    assert(e.getMessage().contains("k choisi superieur a la taille de la liste"));
+			  }
+	}
+	
+	/**
+	 * Indice superieur a la taille de la liste
+	 */
+	public final void testOutOfBound() {
+		CircularList<Candidate> list = new CircularList<>();
+		Candidate test = new Candidate("" , "");
+		list.add(test);
+		 try {
+				list.get(2);
+			    fail("Devrait lever une exception quand l indice de la liste est superieur a sa taille");
+			  }catch(IndexOutOfBoundsException e){
+			    assert(e.getMessage().contains("L index fourni est superieur a la taille de la liste"));
+			  }
 	}
 }
---
+++ exercice_2/src/LauncherExo2.java
@@ -0,0 +1,10 @@
+package src;
+
+import java.util.Random;
+
+public class LauncherExo2 {
+
+	public static void main(String[] args) {
+	}
+
+}
